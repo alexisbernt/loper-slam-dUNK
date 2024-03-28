b4db4e0740d2c786c21cd3e7f7b1228ceac8f5f3
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2024-03-27 21:22:21 -0500

    start of randomizer

--- CODE/RandomizerClass.py
+++ CODE/RandomizerClass.py
@@ -1,2 +1,10 @@
 # This will be the randomizer class
 # Alexis will begin coding the randomizer
+name_list = []
+def select_random_name(name_list):
+    if name_list.size() > 0:
+        random_name = random.choice(name_list.get(0, tk.END))
+        selected_name_var.set(f"Selected Name: {random_name}")
+    else:
+        selected_name_var.set(f"Error. No names entered.")
+        print('Error. No names entered.')
