6dbe4002f8a856fe47e30b55cf763cc440640106
DylNorr1s <dylnorrisj@gmail.com>
2024-04-04 12:12:09 -0500

    Added working textbox to comms class
    As of right now it just saves it to a list defined in the __init__ of Communications.

--- CODE/CommunicationClass.py
+++ CODE/CommunicationClass.py
@@ -8,9 +8,9 @@ class Communication:
     def __init__(self):
         self.window = tk.Tk()
         self.on_screen = []
+        self.announcement_list = []
         self.window.title("Communication")
         self.window.geometry(f'{self.window.winfo_screenwidth() // 4}x{self.window.winfo_screenheight() // 3:.0f}')
-
 
     def reset_for_communicate(self):
         # Clear The Screen_________
@@ -22,6 +22,21 @@ class Communication:
         self.on_screen.append(communication_title)
         communication_title.pack()
 
+        # Adds textbox to comm page, saves it to a list for now
+        comm_textbox = tk.Entry(self.window, width=40)
+        self.on_screen.append(comm_textbox)
+        comm_textbox.pack()
+        send_button = tk.Button(self.window, text="Send announcement",
+                                command=lambda: self.send_announcement(comm_textbox))
+        self.on_screen.append(send_button)
+        send_button.pack()
+
+    def send_announcement(self, comm_textbox):
+        announce = comm_textbox.get()
+        if announce:
+            self.announcement_list.append(announce)
+            comm_textbox.delete(0, tk.END)
+            print("Announcement sent: ", announce)
 
 # run_com_instance = Communication()
 # run_com_instance.reset_for_communicate()
