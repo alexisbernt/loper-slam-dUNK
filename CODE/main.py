7cc670df4a6688fc23b9804ed2f6fe005ac215dc
alexisbernt <129408149+alexisbernt@users.noreply.github.com>
2024-04-09 08:35:34 -0500

    creating login screen

--- CODE/main.py
+++ CODE/main.py
@@ -10,44 +10,72 @@ from tkinter import Label, Button
 from RandomizerClass import RandomClass
 from CommunicationClass import Communication
 from CalendarClass import Calendar
+from Table_Events import EventsController
 
 # Ideas to clear the screen (so we can put everything on the same screen)
-# def __init__(self):
-#     self.window = tk.Tk()
-#     self.on_screen = []
-# Clear The Screen_________
-# for item in self.on_screen:
-#     item.pack_forget()
+# I made a self.on_screen = [] list and that is how we will control
+# what is on the screen at each time. To put something on the screen, append to the list
 
 class SlamdUNK:
     def __init__(self):
         self.window = tk.Tk()
-        self.communication = Communication()  # create instance of the class
-        # JUAN, PLEASE CONNECT YOUR BUTTON TO YOUR LIKING HERE :)
-        currentDate = date.today()
-        self.calendar = Calendar(2004, 3)
-        self.random = RandomClass()  # create instance of the class
-        self.window.title("loper slam dUNK Version 1")
+        self.window.title("loperslamdUNK")
         self.window.geometry(f'{self.window.winfo_screenwidth() // 2}x{self.window.winfo_screenheight() // 1.5:.0f}')
         self.on_screen = []
         self.notes = []
-        # self.open_screen()
-        self.style = Style(theme="litera")  # creating ttkbootstrap style with the specified theme
-        self.style.theme_use('litera')
-        self.dashboard_screen()  # Call dashboard_screen method
-        self.addCalendarEvents()
+        self.sign_in_screen()
+        self.window.mainloop()
 
+    # Clear The Screen Functionalities
+    def clear_screen(self):
+        for widget in self.notes:
+            widget.destroy()
+    def clear_notes(self):
+        for widget in self.notes:
+            widget.destroy()
+        # for item in self.on_screen:
+        #     item.pack_forget()
 
-    def dashboard_screen(self):
-        # Clear The Screen_________
+    def sign_in_screen(self):
         self.clear_screen()
         self.clear_notes()
-        # Title____________________________________________________________
+        sign_in_page_title = Label(self.window, text="Welcome to loperslamdUNK", font=("Ariel",28), pady=10)
+        self.on_screen.append(sign_in_page_title)
+        sign_in_page_title.pack()
+        # FOR USERS TO SIGN IN
+        username_label = Label(self.window, text="Enter username", font=("Ariel", 18))
+        self.on_screen.append(username_label)
+        username_label.pack()
+        username_entry = tk.Entry(self.window)
+        self.on_screen.append(username_entry)
+        username_entry.pack()
+        password_label = Label(self.window, text="Enter password", font=("Ariel", 18))
+        self.on_screen.append(password_label)
+        password_label.pack()
+        password_entry = tk.Entry(self.window)
+        self.on_screen.append(password_entry)
+        password_entry.pack()
+        login_button = Button(self.window, text="Login", font=("Ariel", 15),
+                                command=lambda: self.login(username_entry.get(), password_entry.get()))
+        self.on_screen.append(login_button)
+        login_button.pack()
+
+    def login(self,username, password):
+        # Right now going straight to dashboard screen
+        # Instead hook it up, so it checks to see if username and password
+        # are valid (in database). If so, login, else error.
+        self.dashboard_screen()
+
+    def dashboard_screen(self):
+        # Clear The Screen all over again
+        self.clear_screen()
+        self.clear_notes()
+        # Title
         dashboard_title = Label(self.window, text="DASHBOARD", font=("litera", 25), pady=10)
         self.on_screen.append(dashboard_title)
         dashboard_title.pack()
-
-        # Buttons for navigating to different pages___________________________________________________
+        # You can use the command: command=lambda: self.[call class].[call function]() to connect the button
+        # Buttons for navigating to different pages w/n the application
         communication_button = Button(self.window, text="COMMUNICATE", font=("Ariel", 15),
                                 command=lambda: self.communication.reset_for_communicate())
         self.on_screen.append(communication_button)
@@ -56,19 +84,20 @@ class SlamdUNK:
                                       command=lambda: self.random.reset_for_random())
         self.on_screen.append(random_button)
         random_button.pack()
-        # JUAN, you can edit your button here
-        # You can use the command: command=lambda: self.[call class].[call function]() to connect the button
         calendar_button = Button(self.window, text="CALENDAR", font=("Ariel", 15), command= lambda: self.calendar.show())
         self.on_screen.append(calendar_button)
         calendar_button.pack()
 
-    def clear_screen(self):
-        for item in self.on_screen:
-            item.pack_forget()
-        self.on_screen = []
-
-    def clear_notes(self):
-        self.notes.clear()
+    # self.communication = Communication()  # create instance of the class
+    # # JUAN, PLEASE CONNECT YOUR BUTTON TO YOUR LIKING HERE :)
+    # currentDate = date.today()
+    # self.calendar = Calendar(2004, 3)
+    # self.random = RandomClass()  # create instance of the class
+    # # self.open_screen()
+    # self.style = Style(theme="litera")  # creating ttkbootstrap style with the specified theme
+    # self.style.theme_use('litera')
+    # self.dashboard_screen()  # Call dashboard_screen method
+    # self.addCalendarEvents()
 
     def addCalendarEvents(self):
         events = EventsController()
@@ -79,4 +108,4 @@ class SlamdUNK:
 # Example usage
 slamdunk_instance = SlamdUNK()
 # slamdunk_instance.name_entry.pack_forget()  # hide the entry widget
-slamdunk_instance.window.mainloop()
+# slamdunk_instance.window.mainloop()
